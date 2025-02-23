de7e9f8daec624a714f1b9651df0ea7207d80220
FardinMahadi <mahadihasanfardin2015@gmail.com>
2025-02-23 18:11:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kbm4w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("TechTrails");
+    const usersCollection = database.collection("users");
+    const blogssCollection = database.collection("blogs");
+
+    app.post("/users", async (req, res) => {
+      try {
+        const newUser = req.body;
+        console.log(newUser);
+
+        const result = await usersCollection.insertOne(newUser);
+
+        res.send(result);
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Blog server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on PORT: ${port}`);
+});
